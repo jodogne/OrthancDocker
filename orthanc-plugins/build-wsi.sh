dfb89b65ad52c06b52a7a232f8b6da9369d5a5c1
Sebastien Jodogne <s.jodogne@gmail.com>
2018-04-10 07:25:36 +0200

    revert

--- orthanc-plugins/build-wsi.sh
+++ orthanc-plugins/build-wsi.sh
@@ -36,7 +36,6 @@ mkdir Build
 cd Build
 cmake -DALLOW_DOWNLOADS:BOOL=ON \
     -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DUSE_SYSTEM_DCMTK:BOOL=OFF \
     -DUSE_SYSTEM_JSONCPP:BOOL=OFF \
     ..
 make -j$COUNT_CORES
