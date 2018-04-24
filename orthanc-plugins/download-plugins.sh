4e7cca87a7be7be0cc5e9269beda1ec6f4f19bec
Sebastien Jodogne <s.jodogne@gmail.com>
2018-04-24 10:25:40 +0200

    orthanc and orthanc-plugins are now slim

--- orthanc-plugins/download-plugins.sh
+++ orthanc-plugins/download-plugins.sh
@@ -4,19 +4,12 @@ set -e
 cd
 
 URL=http://lsb.orthanc-server.com/
-VERSION_ORTHANC=mainline
 VERSION_DICOM_WEB=mainline
 VERSION_POSTGRESQL=mainline
 VERSION_WEB_VIEWER=mainline
 VERSION_WSI=mainline
 
 # Download binaries compiled with Linux Standard Base
-wget ${URL}/orthanc/${VERSION_ORTHANC}/Orthanc
-wget ${URL}/orthanc/${VERSION_ORTHANC}/OrthancRecoverCompressedFile
-wget ${URL}/orthanc/${VERSION_ORTHANC}/UnitTests -O - > UnitTests-Orthanc
-wget ${URL}/orthanc/${VERSION_ORTHANC}/libModalityWorklists.so
-wget ${URL}/orthanc/${VERSION_ORTHANC}/libServeFolders.so
-
 wget ${URL}/plugin-dicom-web/${VERSION_DICOM_WEB}/UnitTests -O - > UnitTests-DicomWeb
 wget ${URL}/plugin-dicom-web/${VERSION_DICOM_WEB}/libOrthancDicomWeb.so
 
@@ -31,19 +24,15 @@ wget ${URL}/whole-slide-imaging/${VERSION_WSI}/OrthancWSIDicomToTiff
 wget ${URL}/whole-slide-imaging/${VERSION_WSI}/OrthancWSIDicomizer
 wget ${URL}/whole-slide-imaging/${VERSION_WSI}/libOrthancWSI.so
 
-chmod +x ./Orthanc
-chmod +x ./OrthancRecoverCompressedFile
 chmod +x ./OrthancWSIDicomToTiff
 chmod +x ./OrthancWSIDicomizer
 chmod +x ./UnitTests-DicomWeb
-chmod +x ./UnitTests-Orthanc
 chmod +x ./UnitTests-PostgreSQL
 chmod +x ./UnitTests-WebViewer
 
 # Run the unit tests
 mkdir ~/UnitTests
 cd ~/UnitTests
-../UnitTests-Orthanc
 ../UnitTests-DicomWeb
 # ../UnitTests-PostgreSQL
 ../UnitTests-WebViewer
@@ -52,23 +41,14 @@ cd ~/UnitTests
 cd
 rm -rf ./UnitTests
 rm -rf ./UnitTests-DicomWeb
-rm -rf ./UnitTests-Orthanc
 rm -rf ./UnitTests-PostgreSQL
 rm -rf ./UnitTests-WebViewer
 
 # Move the binaries to their final location
-mkdir -p /usr/local/sbin/
-mkdir -p /usr/local/bin/
-mkdir -p /usr/local/share/orthanc/plugins/
-
-mv ./Orthanc                        /usr/local/sbin/
-mv ./OrthancRecoverCompressedFile   /usr/local/bin/
 mv ./OrthancWSIDicomToTiff          /usr/local/bin/
 mv ./OrthancWSIDicomizer            /usr/local/bin/
-mv ./libModalityWorklists.so        /usr/local/share/orthanc/plugins/
 mv ./libOrthancDicomWeb.so          /usr/local/share/orthanc/plugins/
 mv ./libOrthancPostgreSQLIndex.so   /usr/local/share/orthanc/plugins/
 mv ./libOrthancPostgreSQLStorage.so /usr/local/share/orthanc/plugins/
 mv ./libOrthancWSI.so               /usr/local/share/orthanc/plugins/
 mv ./libOrthancWebViewer.so         /usr/local/share/orthanc/plugins/
-mv ./libServeFolders.so             /usr/local/share/orthanc/plugins/
---
+++ orthanc/download-orthanc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+cd
+
+URL=http://lsb.orthanc-server.com/
+VERSION_ORTHANC=mainline
+
+# Download binaries compiled with Linux Standard Base
+wget ${URL}/orthanc/${VERSION_ORTHANC}/Orthanc
+wget ${URL}/orthanc/${VERSION_ORTHANC}/OrthancRecoverCompressedFile
+wget ${URL}/orthanc/${VERSION_ORTHANC}/UnitTests -O - > UnitTests-Orthanc
+wget ${URL}/orthanc/${VERSION_ORTHANC}/libModalityWorklists.so
+wget ${URL}/orthanc/${VERSION_ORTHANC}/libServeFolders.so
+
+chmod +x ./Orthanc
+chmod +x ./OrthancRecoverCompressedFile
+chmod +x ./UnitTests-Orthanc
+
+# Run the unit tests
+mkdir ~/UnitTests
+cd ~/UnitTests
+../UnitTests-Orthanc
+
+# Recover space used by the unit tests
+cd
+rm -rf ./UnitTests
+rm -rf ./UnitTests-Orthanc
+
+# Move the binaries to their final location
+mkdir -p /usr/local/sbin/
+mkdir -p /usr/local/bin/
+mkdir -p /usr/local/share/orthanc/plugins/
+
+mv ./Orthanc                        /usr/local/sbin/
+mv ./OrthancRecoverCompressedFile   /usr/local/bin/
+mv ./libModalityWorklists.so        /usr/local/share/orthanc/plugins/
+mv ./libServeFolders.so             /usr/local/share/orthanc/plugins/
