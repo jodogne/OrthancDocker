3058eb9395d192dcbba5b2f6ef1b7cf0d227004c
Sebastien Jodogne <s.jodogne@gmail.com>
2018-04-24 10:30:46 +0200

    orthanc and orthanc-plugins are now slim, for 1.3.2

---
+++ orthanc-plugins/download-plugins.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+cd
+
+URL=http://lsb.orthanc-server.com/
+VERSION_DICOM_WEB=0.5
+VERSION_POSTGRESQL=2.1
+VERSION_WEB_VIEWER=2.4
+VERSION_WSI=0.5
+
+# Download binaries compiled with Linux Standard Base
+wget ${URL}/plugin-dicom-web/${VERSION_DICOM_WEB}/UnitTests -O - > UnitTests-DicomWeb
+wget ${URL}/plugin-dicom-web/${VERSION_DICOM_WEB}/libOrthancDicomWeb.so
+
+wget ${URL}/plugin-postgresql/${VERSION_POSTGRESQL}/UnitTests -O - > UnitTests-PostgreSQL
+wget ${URL}/plugin-postgresql/${VERSION_POSTGRESQL}/libOrthancPostgreSQLIndex.so
+wget ${URL}/plugin-postgresql/${VERSION_POSTGRESQL}/libOrthancPostgreSQLStorage.so
+
+wget ${URL}/plugin-webviewer/${VERSION_WEB_VIEWER}/UnitTests -O - > UnitTests-WebViewer
+wget ${URL}/plugin-webviewer/${VERSION_WEB_VIEWER}/libOrthancWebViewer.so
+
+wget ${URL}/whole-slide-imaging/${VERSION_WSI}/OrthancWSIDicomToTiff
+wget ${URL}/whole-slide-imaging/${VERSION_WSI}/OrthancWSIDicomizer
+wget ${URL}/whole-slide-imaging/${VERSION_WSI}/libOrthancWSI.so
+
+chmod +x ./OrthancWSIDicomToTiff
+chmod +x ./OrthancWSIDicomizer
+chmod +x ./UnitTests-DicomWeb
+chmod +x ./UnitTests-PostgreSQL
+chmod +x ./UnitTests-WebViewer
+
+# Run the unit tests
+mkdir ~/UnitTests
+cd ~/UnitTests
+../UnitTests-DicomWeb
+# ../UnitTests-PostgreSQL
+../UnitTests-WebViewer
+
+# Recover space used by the unit tests
+cd
+rm -rf ./UnitTests
+rm -rf ./UnitTests-DicomWeb
+rm -rf ./UnitTests-PostgreSQL
+rm -rf ./UnitTests-WebViewer
+
+# Move the binaries to their final location
+mv ./OrthancWSIDicomToTiff          /usr/local/bin/
+mv ./OrthancWSIDicomizer            /usr/local/bin/
+mv ./libOrthancDicomWeb.so          /usr/local/share/orthanc/plugins/
+mv ./libOrthancPostgreSQLIndex.so   /usr/local/share/orthanc/plugins/
+mv ./libOrthancPostgreSQLStorage.so /usr/local/share/orthanc/plugins/
+mv ./libOrthancWSI.so               /usr/local/share/orthanc/plugins/
+mv ./libOrthancWebViewer.so         /usr/local/share/orthanc/plugins/
