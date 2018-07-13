c3ce5bdc8ddc114d23eb64ff7e2812fbb23bde46
Sebastien Jodogne <s.jodogne@gmail.com>
2018-07-13 15:39:41 +0200

    fix

--- orthanc/setup-locales.sh
+++ orthanc/setup-locales.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y locales wget
 
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-ln -s /etc/locale.alias /usr/share/locale/locale.alias
+# ln -s /etc/locale.alias /usr/share/locale/locale.alias
 locale-gen
 
 rm -rf /var/lib/apt/lists/*
