c599154cabc4c76bd04095ec102cf5fcdb3bd479
Sebastien Jodogne <s.jodogne@gmail.com>
2019-02-08 15:45:11 +0100

    Orthanc 1.5.4

--- orthanc/download-orthanc.sh
+++ orthanc/download-orthanc.sh
@@ -4,7 +4,7 @@ set -e
 cd
 
 URL=http://lsb.orthanc-server.com/
-VERSION_ORTHANC=mainline
+VERSION_ORTHANC=1.5.4
 
 # Download binaries compiled with Linux Standard Base
 wget ${URL}/orthanc/${VERSION_ORTHANC}/Orthanc
