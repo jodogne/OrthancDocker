89c767e783488c7d0fa13e3e11655c4d464cc4de
Sebastien Jodogne <s.jodogne@gmail.com>
2019-10-04 18:19:53 +0200

    let Orthanc create its default user

--- orthanc/create-config.sh
+++ orthanc/create-config.sh
@@ -17,4 +17,11 @@ sed 's/\("StorageDirectory" : \)".*"/\1"\/var\/lib\/orthanc\/db"/' -i $CONFIG
 sed 's/\("Plugins" : \[\)/\1 \n    "\/usr\/share\/orthanc\/plugins", "\/usr\/local\/share\/orthanc\/plugins"/' -i $CONFIG
 sed 's/"RemoteAccessAllowed" : false/"RemoteAccessAllowed" : true/' -i $CONFIG
 sed 's/"AuthenticationEnabled" : false/"AuthenticationEnabled" : true/' -i $CONFIG
-sed 's/\("RegisteredUsers" : {\)/\1\n    "orthanc" : "orthanc"/' -i $CONFIG
+
+
+# Starting with Orthanc 1.5.8, we let Orthanc create its default
+# user. This results in showing a warning about "Insecure setup", both
+# in the logs and in Orthanc Explorer. => The following call to "sed"
+# on "RegisterUsers" must *NOT* be done.
+
+# sed 's/\("RegisteredUsers" : {\)/\1\n    "orthanc" : "orthanc"/' -i $CONFIG
