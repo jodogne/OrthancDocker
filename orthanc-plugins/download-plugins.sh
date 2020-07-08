141a4f57427f7f7a78db88e266f0ef6a824e7ffd
Sebastien Jodogne <s.jodogne@gmail.com>
2020-07-08 08:03:56 +0200

    removing google-cloud

--- orthanc-plugins/download-plugins.sh
+++ orthanc-plugins/download-plugins.sh
@@ -7,7 +7,6 @@ URL=http://lsb.orthanc-server.com/
 
 VERSION_AUTHORIZATION=0.2.3
 VERSION_DICOM_WEB=1.2
-VERSION_GCP=1.0
 VERSION_GDCM=1.0
 VERSION_MYSQL=2.0
 VERSION_POSTGRESQL=3.2
@@ -43,8 +42,6 @@ wget ${URL}/whole-slide-imaging/${VERSION_WSI}/OrthancWSIDicomizer
 wget ${URL}/whole-slide-imaging/${VERSION_WSI}/libOrthancWSI.so
 
 wget ${URL}/plugin-authorization/${VERSION_AUTHORIZATION}/libOrthancAuthorization.so
-
-wget ${URL}/plugin-google-cloud/${VERSION_GCP}/libOrthancGoogleCloudPlatform.so
 
 wget ${URL}/plugin-gdcm/${VERSION_GDCM}/libOrthancGdcm.so
 
@@ -86,5 +83,4 @@ mv ./libOrthancTransfers.so            /usr/local/share/orthanc/plugins/
 mv ./libOrthancWSI.so                  /usr/local/share/orthanc/plugins/
 mv ./libOrthancWebViewer.so            /usr/local/share/orthanc/plugins/
 mv ./libOrthancAuthorization.so        /usr/local/share/orthanc/plugins/
-mv ./libOrthancGoogleCloudPlatform.so  /usr/local/share/orthanc/plugins/
 mv ./libOrthancGdcm.so                 /usr/local/share/orthanc/plugins/
