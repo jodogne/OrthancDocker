099829e94869153373df220fc7f7855105e84c65
Sebastien Jodogne <s.jodogne@gmail.com>
2021-08-27 14:09:29 +0200

    enabling libRtViewerPlugin.so in release images

--- orthanc-plugins/download-plugins.sh
+++ orthanc-plugins/download-plugins.sh
@@ -15,7 +15,7 @@ VERSION_TRANSFERS=1.0
 VERSION_WEB_VIEWER=2.7
 VERSION_WSI=1.0
 VERSION_STONE_WEB_VIEWER=2.1
-# VERSION_STONE_RT_SAMPLE=mainline
+VERSION_STONE_RT_SAMPLE=mainline
 
 # Download binaries compiled with Linux Standard Base
 
@@ -53,7 +53,7 @@ wget ${URL}/plugin-authorization/${VERSION_AUTHORIZATION}/libOrthancAuthorizatio
 wget ${URL}/plugin-gdcm/${VERSION_GDCM}/libOrthancGdcm.so
 
 wget ${URL}/stone-webviewer/${VERSION_STONE_WEB_VIEWER}/libStoneWebViewer.so
-# wget ${URL}/stone-rt-sample/${VERSION_STONE_RT_SAMPLE}/libRtViewerPlugin.so
+wget ${URL}/stone-rt-sample/${VERSION_STONE_RT_SAMPLE}/libRtViewerPlugin.so
 
 chmod +x ./OrthancWSIDicomToTiff
 chmod +x ./OrthancWSIDicomizer
@@ -100,4 +100,4 @@ mv ./libOrthancWebViewer.so            /usr/local/share/orthanc/plugins/
 mv ./libOrthancAuthorization.so        /usr/local/share/orthanc/plugins/
 mv ./libOrthancGdcm.so                 /usr/local/share/orthanc/plugins/
 mv ./libStoneWebViewer.so              /usr/local/share/orthanc/plugins/
-# mv ./libRtViewerPlugin.so              /usr/local/share/orthanc/plugins/
+mv ./libRtViewerPlugin.so              /usr/local/share/orthanc/plugins/
