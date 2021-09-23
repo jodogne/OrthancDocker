282a32d1836598c2bddc1f7aeb54b87c1baa57e9
Sebastien Jodogne <s.jodogne@gmail.com>
2021-09-23 13:32:13 +0200

    added indexer plugin

--- orthanc-plugins/download-plugins.sh
+++ orthanc-plugins/download-plugins.sh
@@ -8,6 +8,7 @@ URL=http://lsb.orthanc-server.com/
 VERSION_AUTHORIZATION=0.2.4
 VERSION_DICOM_WEB=1.7
 VERSION_GDCM=1.4
+VERSION_INDEXER=mainline
 VERSION_MYSQL=4.3
 VERSION_ODBC=1.0
 VERSION_POSTGRESQL=4.0
@@ -24,7 +25,7 @@ VERSION_STONE_RT_SAMPLE=mainline
 # anymore, as they are now built using the Holy Build Box because of
 # incompatibility between GDCM 3.0 and LSB compilers
 
-#wget ${URL}/plugin-dicom-web/${VERSION_DICOM_WEB}/UnitTests -O - > UnitTests-DicomWeb
+wget ${URL}/plugin-dicom-web/${VERSION_DICOM_WEB}/UnitTests -O - > UnitTests-DicomWeb
 wget ${URL}/plugin-dicom-web/${VERSION_DICOM_WEB}/libOrthancDicomWeb.so
 
 wget ${URL}/plugin-postgresql/${VERSION_POSTGRESQL}/UnitTests -O - > UnitTests-PostgreSQL
@@ -42,7 +43,7 @@ wget ${URL}/plugin-odbc/${VERSION_ODBC}/libOrthancOdbcStorage.so
 wget ${URL}/plugin-transfers/${VERSION_TRANSFERS}/UnitTests -O - > UnitTests-Transfers
 wget ${URL}/plugin-transfers/${VERSION_TRANSFERS}/libOrthancTransfers.so
 
-#wget ${URL}/plugin-webviewer/${VERSION_WEB_VIEWER}/UnitTests -O - > UnitTests-WebViewer
+wget ${URL}/plugin-webviewer/${VERSION_WEB_VIEWER}/UnitTests -O - > UnitTests-WebViewer
 wget ${URL}/plugin-webviewer/${VERSION_WEB_VIEWER}/libOrthancWebViewer.so
 
 wget ${URL}/whole-slide-imaging/${VERSION_WSI}/OrthancWSIDicomToTiff
@@ -55,27 +56,32 @@ wget ${URL}/plugin-gdcm/${VERSION_GDCM}/libOrthancGdcm.so
 
 wget ${URL}/plugin-tcia/${VERSION_TCIA}/libOrthancTcia.so
 
+wget ${URL}/plugin-indexer/${VERSION_INDEXER}/libOrthancIndexer.so
+wget ${URL}/plugin-indexer/${VERSION_INDEXER}/UnitTests -O - > UnitTests-Indexer
+
 wget ${URL}/stone-webviewer/${VERSION_STONE_WEB_VIEWER}/libStoneWebViewer.so
 wget ${URL}/stone-rt-sample/${VERSION_STONE_RT_SAMPLE}/libRtViewerPlugin.so
 
 chmod +x ./OrthancWSIDicomToTiff
 chmod +x ./OrthancWSIDicomizer
-#chmod +x ./UnitTests-DicomWeb
+chmod +x ./UnitTests-DicomWeb
 chmod +x ./UnitTests-PostgreSQL
 chmod +x ./UnitTests-MySQL
 chmod +x ./UnitTests-Odbc
 chmod +x ./UnitTests-Transfers
-#chmod +x ./UnitTests-WebViewer
+chmod +x ./UnitTests-WebViewer
+chmod +x ./UnitTests-Indexer
 
 # Run the unit tests
 mkdir ~/UnitTests
 cd ~/UnitTests
-#../UnitTests-DicomWeb
+../UnitTests-DicomWeb
 # ../UnitTests-PostgreSQL
 # ../UnitTests-MySQL
 # ../UnitTests-Odbc
 ../UnitTests-Transfers
-#../UnitTests-WebViewer
+../UnitTests-WebViewer
+../UnitTests-Indexer
 
 # Recover space used by the unit tests
 cd
@@ -86,6 +92,7 @@ rm -rf ./UnitTests-MySQL
 rm -rf ./UnitTests-Odbc
 rm -rf ./UnitTests-Transfers
 rm -rf ./UnitTests-WebViewer
+rm -rf ./UnitTests-Indexer
 
 # Move the binaries to their final location
 mv ./OrthancWSIDicomToTiff             /usr/local/bin/
@@ -103,5 +110,6 @@ mv ./libOrthancWebViewer.so            /usr/local/share/orthanc/plugins/
 mv ./libOrthancAuthorization.so        /usr/local/share/orthanc/plugins/
 mv ./libOrthancGdcm.so                 /usr/local/share/orthanc/plugins/
 mv ./libOrthancTcia.so                 /usr/local/share/orthanc/plugins/
+mv ./libOrthancIndexer.so              /usr/local/share/orthanc/plugins/
 mv ./libStoneWebViewer.so              /usr/local/share/orthanc/plugins/
 mv ./libRtViewerPlugin.so              /usr/local/share/orthanc/plugins/
