fdd48fcbd2ab122dce81a06e0dd250604f9eb01e
Sébastien Jodogne <s.jodogne@gmail.com>
2023-12-22 15:04:14 +0100

    Orthanc 1.12.2

--- orthanc/download-orthanc.sh
+++ orthanc/download-orthanc.sh
@@ -4,7 +4,7 @@ set -e
 cd
 
 URL=https://orthanc.uclouvain.be/downloads/linux-standard-base
-VERSION_ORTHANC=mainline
+VERSION_ORTHANC=1.12.2
 
 # Download binaries compiled with Linux Standard Base
 wget ${URL}/orthanc/${VERSION_ORTHANC}/Orthanc
