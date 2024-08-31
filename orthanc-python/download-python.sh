69d942740a806e9a6103a1d10b53be57b62e7e5b
Sébastien Jodogne <s.jodogne@gmail.com>
2024-08-31 10:20:06 +0200

    fix

--- orthanc-python/download-python.sh
+++ orthanc-python/download-python.sh
@@ -6,6 +6,6 @@ cd
 URL=https://orthanc.uclouvain.be/downloads/linux-standard-base
 VERSION=mainline
 
-wget ${URL}/orthanc-python/debian-buster-python-3.7/${VERSION}/libOrthancPython.so
+wget ${URL}/orthanc-python/debian-buster-python-3.11/${VERSION}/libOrthancPython.so
 
 mv ./libOrthancPython.so  /usr/local/share/orthanc/plugins/
