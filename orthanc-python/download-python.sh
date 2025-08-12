717bccf0bedc25c76a7cb6923360c3693ab2cab1
Sébastien Jodogne <s.jodogne@gmail.com>
2025-08-12 20:10:58 +0200

    OrthancPython-6.0

--- orthanc-python/download-python.sh
+++ orthanc-python/download-python.sh
@@ -4,7 +4,7 @@ set -e
 cd
 
 URL=https://orthanc.uclouvain.be/downloads/linux-standard-base
-VERSION=5.0
+VERSION=6.0
 
 wget ${URL}/orthanc-python/debian-bookworm-python-3.11/${VERSION}/libOrthancPython.so
 
